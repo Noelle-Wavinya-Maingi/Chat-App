990fe082e7b43b369294a0d22196dfcb6ba469c3
Noelle-Wavinya-Maingi <noemaingi@gmail.com>
2024-05-18 09:39:21 +0300

    Created context api and added it to the register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,20 +4,20 @@ import Login from "./Pages/Login";
 import Register from "./Pages/Register";
 import Navbar from "./Components/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {Container} from "react-bootstrap"
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
     <>
-    <Navbar />
-    <Container>
-      <Routes>
-        <Route path="/" element={<Chat />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </Container>
+      <Navbar />
+      <Container>
+        <Routes>
+          <Route path="/" element={<Chat />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Container>
     </>
   );
 }
